6ab70c838ec83cce8e962582b7f784bfaeee03e9
Bohdan Yurov <bohdan@yurov.me>
2020-04-09 15:58:59 +0300

    Fargate example

--- locals.tf
+++ locals.tf
@@ -13,15 +13,36 @@ locals {
     }
 
     apps = {
-      demo_eks_ec2         = {
+      demo_eks_ec2     = {
         name        = "demo-ec2"
         namespace   = var.k8s_namespace_demo_ec2
-        helm_values = {}
+        helm_values = {
+          image = {
+            repository = "tutum/hello-world"
+          }
+        }
       }
       demo_eks_fargate = {
         name        = "demo-fargate"
-        namespace = var.k8s_namespace_demo_fargate
-        helm_values = {}
+        image       = "tutum/hello-world"
+        namespace   = var.k8s_namespace_demo_fargate
+        helm_values = {
+          image = {
+            repository = "tutum/hello-world"
+          }
+        }
+      }
+      demo_fargate     = {
+        name    = "demo"
+        image   = "tutum/hello-world"
+        cluster = aws_ecs_cluster.this.name
+        subnets = module.vpc.public_subnets
+        ports   = [
+          {
+            containerPort = 80
+            hostPort      = 80
+          }
+        ]
       }
     }
   }
